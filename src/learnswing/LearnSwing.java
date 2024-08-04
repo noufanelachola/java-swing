e4c91da7eb66486251845a0dc419b7f0c6b8d9b1
noufanelachola <noufansadiqelachola@gmail.com>
2024-08-04 23:46:30 +0530

    first commit

---
+++ src/learnswing/LearnSwing.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package learnswing;
+import myframe.MyFrame;
+
+
+
+public class LearnSwing {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyFrame frame = new MyFrame();
+        
+    }
+    
+}
---
+++ src/myframe/MyFrame.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myframe;
+
+/**
+ *
+ * @author USER
+ */
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame(){
+        
+        ImageIcon image = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\IMG-20201026-WA0383.jpg");
+        
+        JLabel label = new JLabel("Hello");
+        label.setIcon(image);
+        
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        
+        label.setForeground(Color.red);
+        label.setFont(new Font("Poppins", Font.PLAIN, 20));
+        
+        this.setTitle("JFrame");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setVisible(true);
+        this.add(label);
+        
+        ImageIcon icon = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\logo2.png");
+        this.setIconImage(icon.getImage());
+
+        this.getContentPane().setBackground(new Color(0xFFFFFF));
+    }
+    
+}
