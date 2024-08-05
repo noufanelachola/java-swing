701cf46f166080c49c10d3b6fdd73a0560f3f8f3
noufanelachola <noufansadiqelachola@gmail.com>
2024-08-06 04:33:25 +0530

    JLabel

--- src/myframe/MyFrame.java
+++ src/myframe/MyFrame.java
@@ -14,13 +14,15 @@ import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
 
 public class MyFrame extends JFrame {
 
     public MyFrame(){
         
         ImageIcon image = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\IMG-20201026-WA0383.jpg");
-        
+        Border border = BorderFactory.createLineBorder(Color.green,3);
         JLabel label = new JLabel("Hello");
         label.setIcon(image);
         
@@ -30,9 +32,21 @@ public class MyFrame extends JFrame {
         label.setForeground(Color.red);
         label.setFont(new Font("Poppins", Font.PLAIN, 20));
         
+        label.setIconTextGap(0);
+        
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+        
+        label.setBorder(border);
+        
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        
+        //label.setBounds(0,0,600,800);
+        
         this.setTitle("JFrame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
+        //this.setSize(500,800);
         this.setVisible(true);
         this.add(label);
         
@@ -40,6 +54,9 @@ public class MyFrame extends JFrame {
         this.setIconImage(icon.getImage());
 
         this.getContentPane().setBackground(new Color(0xFFFFFF));
+        
+        //this.setLayout(null);
+        this.pack();
     }
     
 }
