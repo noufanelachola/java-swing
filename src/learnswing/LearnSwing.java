21c119f1cf4f4cd3b4ad17ca912be76fd8c2e19e
noufanelachola <noufansadiqelachola@gmail.com>
2024-08-22 00:16:12 +0530

    JPane

--- src/learnswing/LearnSwing.java
+++ src/learnswing/LearnSwing.java
@@ -3,16 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package learnswing;
-import myframe.MyFrame;
-
+//import myframe.MyFrame;
+import javax.swing.*;
+import java.awt.*;
 
 
 public class LearnSwing {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        MyFrame frame = new MyFrame();
-        
+        // MyFrame frame = new MyFrame();
+        ImageIcon icon = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\logo2.png");
+        JLabel label = new JLabel("Hi");
+        label.setIcon(icon);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        panel.setBounds(0, 0, 300, 300);
+
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.red);
+        panel2.setBounds(300, 0, 300, 300);
+
+        JPanel panel3 = new JPanel();
+        panel3.setBackground(Color.GREEN);
+        panel3.setBounds(0, 300, 600, 300);
+
+        JFrame frame = new JFrame("Hello");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750,750);
+        frame.setVisible(true);
+        frame.add(panel);
+        panel.add(label);
+        frame.add(panel2);
+        frame.add(panel3);
+        frame.setIconImage(icon.getImage());
+
     }
     
 }
--- src/myframe/MyFrame.java
+++ src/myframe/MyFrame.java
@@ -21,42 +21,42 @@ public class MyFrame extends JFrame {
 
     public MyFrame(){
         
-        ImageIcon image = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\IMG-20201026-WA0383.jpg");
-        Border border = BorderFactory.createLineBorder(Color.green,3);
-        JLabel label = new JLabel("Hello");
-        label.setIcon(image);
-        
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        
-        label.setForeground(Color.red);
-        label.setFont(new Font("Poppins", Font.PLAIN, 20));
-        
-        label.setIconTextGap(0);
-        
-        label.setBackground(Color.BLACK);
-        label.setOpaque(true);
-        
-        label.setBorder(border);
-        
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
-        
-        //label.setBounds(0,0,600,800);
-        
-        this.setTitle("JFrame");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //this.setSize(500,800);
-        this.setVisible(true);
-        this.add(label);
-        
-        ImageIcon icon = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\logo2.png");
-        this.setIconImage(icon.getImage());
-
-        this.getContentPane().setBackground(new Color(0xFFFFFF));
-        
-        //this.setLayout(null);
-        this.pack();
+//        ImageIcon image = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\IMG-20201026-WA0383.jpg");
+//        Border border = BorderFactory.createLineBorder(Color.green,3);
+//        JLabel label = new JLabel("Hello");
+//        label.setIcon(image);
+//
+//        label.setHorizontalTextPosition(JLabel.CENTER);
+//        label.setVerticalTextPosition(JLabel.TOP);
+//
+//        label.setForeground(Color.red);
+//        label.setFont(new Font("Poppins", Font.PLAIN, 20));
+//
+//        label.setIconTextGap(0);
+//
+//        label.setBackground(Color.BLACK);
+//        label.setOpaque(true);
+//
+//        label.setBorder(border);
+//
+//        label.setHorizontalAlignment(JLabel.CENTER);
+//        label.setVerticalAlignment(JLabel.CENTER);
+//
+//        //label.setBounds(0,0,600,800);
+//
+//        this.setTitle("JFrame");
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        //this.setSize(500,800);
+//        this.setVisible(true);
+//        this.add(label);
+//
+//        ImageIcon icon = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\logo2.png");
+//        this.setIconImage(icon.getImage());
+//
+//        this.getContentPane().setBackground(new Color(0xFFFFFF));
+//
+//        //this.setLayout(null);
+//        this.pack();
     }
     
 }
