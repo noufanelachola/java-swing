d6a7d13b2c1d5b86db5e1a110b6a5f9b18b83c54
noufanelachola <noufansadiqelachola@gmail.com>
2024-08-22 06:45:38 +0530

    Jbuttons

--- src/learnswing/LearnSwing.java
+++ src/learnswing/LearnSwing.java
@@ -6,39 +6,51 @@ package learnswing;
 //import myframe.MyFrame;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 
-public class LearnSwing {
+public class LearnSwing implements ActionListener {
     
     public static void main(String[] args) {
         // TODO code application logic here
         // MyFrame frame = new MyFrame();
         ImageIcon icon = new ImageIcon("D:\\Codes\\Java\\Swing\\LearnSwing\\src\\myframe\\logo2.png");
         JLabel label = new JLabel("Hi");
+        JButton button = new JButton("Click Me");
+
+        button.setBounds(100,100,200,200);
+
         label.setIcon(icon);
 
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.BLUE);
-        panel.setBounds(0, 0, 300, 300);
-
-        JPanel panel2 = new JPanel();
-        panel2.setBackground(Color.red);
-        panel2.setBounds(300, 0, 300, 300);
-
-        JPanel panel3 = new JPanel();
-        panel3.setBackground(Color.GREEN);
-        panel3.setBounds(0, 300, 600, 300);
+//        JPanel panel = new JPanel();
+//        panel.setBackground(Color.BLUE);
+//        panel.setBounds(0, 0, 300, 300);
+//
+//        JPanel panel2 = new JPanel();
+//        panel2.setBackground(Color.red);
+//        panel2.setBounds(300, 0, 300, 300);
+//
+//        JPanel panel3 = new JPanel();
+//        panel3.setBackground(Color.GREEN);
+//        panel3.setBounds(0, 300, 600, 300);
 
         JFrame frame = new JFrame("Hello");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(750,750);
         frame.setVisible(true);
-        frame.add(panel);
-        panel.add(label);
-        frame.add(panel2);
-        frame.add(panel3);
+//        frame.add(panel);
+//        panel.add(label);
+//        frame.add(panel2);
+//        frame.add(panel3);
+        frame.add(button);
         frame.setIconImage(icon.getImage());
+
+
+        @Override
+        public void actionPerformed(ActionEvent e){
+            if (e.getSource())
+        }
 
     }
     
