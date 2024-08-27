dc25c90891fc42971037b4659b02efb1a26c5d63
noufanelachola <noufansadiqelachola@gmail.com>
2024-08-27 06:14:55 +0530

    brder layout completed

--- src/SwingBorderLayout/SwingBorderLayout.java
+++ src/SwingBorderLayout/SwingBorderLayout.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class SwingBorderLayout extends JFrame {
     public SwingBorderLayout(){
 //        JFrame frame = new JFrame();
-        this.setLayout(new BorderLayout());
+        this.setLayout(new BorderLayout(10,5));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("SwingBorderLayout");
         this.setVisible(true);
@@ -24,6 +24,8 @@ public class SwingBorderLayout extends JFrame {
         panel4.setBackground(Color.yellow);
         panel5.setBackground(Color.orange);
 
+        panel2.setLayout(new BorderLayout(5,5));
+
         panel1.setPreferredSize(new Dimension(100,100));
         panel2.setPreferredSize(new Dimension(100,100));
         panel3.setPreferredSize(new Dimension(100,100));
@@ -35,6 +37,31 @@ public class SwingBorderLayout extends JFrame {
         this.add(panel3,BorderLayout.SOUTH);
         this.add(panel4,BorderLayout.EAST);
         this.add(panel5,BorderLayout.WEST);
+
+        //------------------------SUB-PANELS-----------------------------
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(Color.red);
+        panel7.setBackground(Color.green);
+        panel8.setBackground(Color.blue);
+        panel9.setBackground(Color.yellow);
+        panel10.setBackground(Color.orange);
+
+        panel6.setPreferredSize(new Dimension(100,100));
+        panel7.setPreferredSize(new Dimension(100,100));
+        panel8.setPreferredSize(new Dimension(100,100));
+        panel9.setPreferredSize(new Dimension(100,100));
+        panel10.setPreferredSize(new Dimension(100,100));
+
+        panel2.add(panel6,BorderLayout.NORTH);
+        panel2.add(panel7,BorderLayout.CENTER);
+        panel2.add(panel8,BorderLayout.SOUTH);
+        panel2.add(panel9,BorderLayout.EAST);
+        panel2.add(panel10,BorderLayout.WEST);
 
     }
 }
